b1c3d97aeec7554bf15b6847804e07bc84146a62
Boonyarit Sanguansataya <boons505@postbox.in.th>
2025-03-06 22:23:05 +0700

    Add main method with test cases for examples 1 and 2

--- Q1StockProfit.java
+++ Q1StockProfit.java
@@ -13,4 +13,14 @@ public class Q1StockProfit {
         }
         return maxProfit;
     }
+
+    public static void main(String[] args) {
+        Q1StockProfit solution = new Q1StockProfit();
+        // Test Example 1
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        System.out.println("Example 1: " + solution.maxProfit(prices1));
+        // Test Example 2
+        int[] prices2 = {7, 6, 4, 3, 1};
+        System.out.println("Example 2: " + solution.maxProfit(prices2));
+    }
 }
